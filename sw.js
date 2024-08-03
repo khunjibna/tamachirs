27bc6d366a9af1a84c5d6f7ef2dc3cea4011543c
khunjibna <143615272+khunjibna@users.noreply.github.com>
2024-08-03 15:56:02 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-
-    importScripts("https://cdn.npubpro.com/index.js");
-    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1721971158601" }, { url: "/sw.js", revision: "1721971158602" }, { url: "/manifest.webmanifest", revision: "1721971158603" }] });
-  
